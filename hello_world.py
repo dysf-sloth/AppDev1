9061d6ddfea8dae8f7bef766d2f9d1489e63a989
dysf-sloth <slothsrule91@gmail.com>
2024-12-09 05:31:12 -0800

    frame exercise

--- hello_world.py
+++ hello_world.py
@@ -1,21 +1,26 @@
 import tkinter as tk    
 
 window=tk.Tk()
-window.title("Python GUI with Tkinter - Layouts")
+window.title("Python GUI with Tkinter - Frames")
+top_frame = tk.Frame(window)
+bottom_frame = tk.Frame(window)
+
+top_frame.pack()
+bottom_frame.pack()
 
 #create widgets here
 
-text1 = tk.Label(window, text="this application tests simple layout")
-button1 = tk.Button(text="Button 1", fg="green")
-button2 = tk.Button(text="Button 2", fg="red")
-button3 = tk.Button(text="Button 3", fg = "blue")
+text1 = tk.Label(top_frame, text="this application tests frame layout")
+button1 = tk.Button(bottom_frame, text="Button 1", fg="green")
+button2 = tk.Button(bottom_frame, text="Button 2", fg="red")
+button3 = tk.Button(bottom_frame, text="Button 3", fg = "blue")
 
 #pack widgets
 
 text1.pack()
-button1.pack()
-button2.pack()
-button3.pack()
+button1.pack(side= tk.LEFT)
+button2.pack(side= tk.LEFT)
+button3.pack(side= tk.LEFT)
 
 # Start the main event loop
 window.mainloop()
